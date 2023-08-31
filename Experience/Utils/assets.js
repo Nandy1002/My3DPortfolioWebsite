cd552944399161c438c965d0fe20a4caf65bc088
Nabendu Das <72450265+Nandy1002@users.noreply.github.com>
2023-09-01 04:45:31 +0530

    added files

---
+++ Experience/Utils/assets.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        name: "room",
+        type: "glbModel",
+        path: "/models/Finale Version.glb",
+    },
+    {
+        name: "screen",
+        type: "videoTexture",
+        path: "/textures/unity.mp4",
+    },
+];
